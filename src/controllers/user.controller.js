59e1e8ae6a98a4806eb4b01cf51e7fc4c1b6ad26
Chanchal010 <Chanchalbag112@gmail.com>
2024-04-29 06:45:06 +0000

    inject middleware and start conntroller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,14 +1,29 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-
-
+import { User } from "../models/user.models.js"
+import { ApiError } from "../utils/apiError.js"
 const registerUser = asyncHandler(async (req,res) => {
-    res
-    .status(201)
-    .json({
-        message:"Success"
-    })
-})
+    // get user details from frontend
+    // validation -- check user details are not empty
+    // check if the user already exits: (email, username)
+    // check for images and avatar
+    // upload them to cloudinary, avatar
+    // create user object - create entry in DB
+    // remove password and refresh token field from res
+    // check user creation 
+    // return res
 
+    const {fullName, userName, email, password} = req.body
+    console.log(fullName, userName, email, password);
+
+    if(
+        [fullName, userName, email, password].some((fields) => 
+        fields?.trim() === "")
+    ){
+        throw new ApiError(401, "all details are required")
+    }
+
+    
+})
 export {
     registerUser,
 }
--- src/models/user.models.js
+++ src/models/user.models.js
@@ -58,7 +58,7 @@ userSchema.pre("save", async function (next){
     this.password = bcrypt.hash(this.password, 10)
     next()
 })
-//methods use for creating methods...
+//methods. use for creating methods...
 userSchema.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password,this.password)
 }
