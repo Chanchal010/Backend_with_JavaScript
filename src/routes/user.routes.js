e7fa89d63fc7242dfd9ad2ec51f716dfea32df97
Chanchal010 <Chanchalbag112@gmail.com>
2024-05-05 13:45:41 +0000

    checking

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,21 +1,21 @@
 import { Router } from "express";
 import { loginUser,  logoutUser,  registerUser } from "../controllers/user.controller.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
-import { upload } from "../middlewares/multer.middleware.js";
+// import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router()
 
 router.route("/register").post(
-    upload.fields([
-        {
-            name: "avatar",
-            maxCount: 1
-        },
-        {
-            name: "coverImage",
-            maxCount: 1
-        }
-    ]),
+    // upload.fields([
+    //     {
+    //         name: "avatar",
+    //         maxCount: 1
+    //     },
+    //     {
+    //         name: "coverImage",
+    //         maxCount: 1
+    //     }
+    // ]),
     
     registerUser
 )
