812e12eeebb127e4bd9d4b07b4c0939f67cb1617
Chanchal010 <Chanchalbag112@gmail.com>
2024-05-08 14:44:22 +0000

    routes config for controllers

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,15 @@
 import { Router } from "express";
-import { loginUser,  logoutUser,  refreshAccessToken,  registerUser } from "../controllers/user.controller.js";
+import {
+    changeCurrentUserPassword,
+    getCurrentUser,
+    loginUser,
+    logoutUser,
+    refreshAccessToken,
+    registerUser,
+    updateAvatar,
+    updateCoverImage,
+    updateUserDetails
+} from "../controllers/user.controller.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
@@ -16,13 +26,23 @@ router.route("/register").post(
             maxCount: 1
         }
     ]),
-    
+
     registerUser
 )
 
-router.route("/login").post( loginUser )
+router.route("/login").post(loginUser)
 // secured Routes
-router.route("/logout").post( verifyJwt, logoutUser )
+router.route("/logout").post(verifyJwt, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+
+router.route("/change-password").post
+    (verifyJwt, changeCurrentUserPassword)
+router.route("/current-user").get(verifyJwt, getCurrentUser)
+router.route("/update-account").patch(verifyJwt, updateUserDetails)
+
+router.route("/avatar").patch(verifyJwt, upload.single("avatar"), updateAvatar)
+router.route("/cover-image").patch(verifyJwt, upload.single("coverImage"), updateCoverImage)
+
+
 
 export default router
